ed5a6053226328b45bd8c5fcba6c6d90bbcd00f8
Jack Walker <jack@foresightmobile.com>
2019-07-16 13:30:18 +0100

    no message

--- Package.swift
+++ Package.swift
@@ -5,9 +5,10 @@ let package = Package(
 	name: "RxRealmDataSources",
 	dependencies: [
 		.package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("master")),
-		.package(url: "https://github.com/foresightmobile/RxRealm.git", .branch("removed-realm")),
+		.package(url: "https://github.com/foresightmobile/RxRealm", .branch("removed-realm")),
+        .package(url: "https://github.com/realm/realm-cocoa", .branch("tg/spm"))
 	],
     targets: [
-        .target(name: "RxRealmDataSources", dependencies: ["Pod", "RxSwift"], path: ".", sources: ["RxRealmDataSources"])
+        .target(name: "RxRealmDataSources", dependencies: ["Pod", "RxSwift", "RealmSwift"], path: ".", sources: ["RxRealmDataSources"])
     ]
 )
